9a65a1b0c8cc1f52068eb11f2d2387018cd50dca
sptin2002 <sptin2002@gmail.com>
2024-03-31 14:38:24 +0800

    Added User entity and UserRepo repository

---
+++ src/main/java/com/fitness/fitness/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.fitness.fitness.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fitness.fitness.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+    
+}
