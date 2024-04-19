c8e31efb2589577fe4a5570dd08a5c0db5ab1347
jasontjiunardi <zhoujiesheng55@gmail.com>
2024-04-19 17:07:02 +0800

    view trainer html + css

--- src/main/java/com/fitness/fitness/service/TrainerService.java
+++ src/main/java/com/fitness/fitness/service/TrainerService.java
@@ -4,22 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 
 import com.fitness.fitness.model.Trainer;
 import com.fitness.fitness.repository.TrainerRepo;
-=======
-import com.fitness.fitness.repository.TrainerRepo;
-import com.fitness.fitness.model.Trainer;
-import java.util.List;
-
->>>>>>> 56a0767345033be674f69e8187e16f3e4d3bcebb
 @Service
 public class TrainerService {
     @Autowired
     private TrainerRepo trainerRepo;
 
-<<<<<<< HEAD
     public List<Trainer> getAll(){
         return trainerRepo.findAll();
     }
@@ -30,18 +22,4 @@ public class TrainerService {
         // TODO Auto-generated method stub
         return trainerRepo.findAllByRank(rank);
     }
-=======
-    // You can add methods to perform operations using trainerRepo
-
-    public List<Trainer> findAllByRank(int rank) {
-        return trainerRepo.findAllByRank(rank);
-    }
-
-    public List<Trainer> getAllTrainers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllTrainers'");
-    }
-
-    // You can add more methods here depending on your business logic
->>>>>>> 56a0767345033be674f69e8187e16f3e4d3bcebb
 }
