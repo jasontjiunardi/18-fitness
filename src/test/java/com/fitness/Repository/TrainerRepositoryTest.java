446aabe9783e126d53c3b0713dcdc9c3aab43216
jasontjiunardi <zhoujiesheng55@gmail.com>
2024-04-28 01:47:10 +0800

    Fixed uploading images to the server

---
+++ src/test/java/com/fitness/Repository/TrainerRepositoryTest.java
@@ -0,0 +1,17 @@
+// package com.fitness.Repository;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// import com.fitness.fitness.model.Trainer;
+// import com.fitness.fitness.repository.TrainerRepo;
+
+// public class TrainerRepositoryTest {
+//     @Autowired
+//     private TrainerRepo trainerRepository;
+
+//     @Test
+//     public void testCreateTrainer() {
+//          Trainer trainer5 = new Trainer("Jill Wozby", 25, "Female", "1998-02-01", "jwoz@gmail.com", "trainer5.jpg", "278997683", 3, "2024-02-01");
+//     }
+// }
